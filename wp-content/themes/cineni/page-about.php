c2763a008e499f035dfd395ee7ca57be0ea166a1
EscapeRPG <syrphin@syrphin.com>
2025-09-18 16:57:52 +0200

    youhou

--- wp-content/themes/cineni/page-about.php
+++ wp-content/themes/cineni/page-about.php
@@ -1,16 +1,14 @@
-<?php
-get_header(); ?>
+<?php get_header(); ?>
 
 <h1 class="big-title biggest" style="margin-bottom: 0">ABOUT</h1>
 
-<h1 class="big-title" style="margin-top: 0"><div class="title-right">ENI CODE SAGA</div></h1>
+<h1 class="big-title" style="margin-top: 0">
+    <div class="title-right">ENI CODE SAGA</div>
+</h1>
 
-
-<main>
-    <?php the_content() ?>
-</main>
-
-
+<div class="about-description">
+<?php the_content() ?>
+</div>
 
 <?php include('includes/festitle.php'); ?>
 
